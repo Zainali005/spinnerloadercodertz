b8fe6e9ccd6372551c85afbc93f28823b1a6f5b9
Zainali005 <zainali5002@gmail.com>
2024-09-13 15:56:25 +0500

    FINAL UPDATE

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -7,10 +7,8 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        // user: process.env.EMAIL_USER,
-        // pass: process.env.EMAIL_PASS,
-        user:"zainali5002@gmail.com",
-        password:"aczkmjkcatkbyxjc"
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
