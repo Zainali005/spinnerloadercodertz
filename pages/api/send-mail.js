a6bd6865e0a9507e17e548b70ed4d0096407e216
Zain Ali <102908271+Zainali005@users.noreply.github.com>
2024-09-16 09:58:56 +0500

    async await

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -7,14 +7,14 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: "zainali5002@gmail.com",
+        pass: "aczkmjkcatkbyxjc",
       },
     });
 
     const mailOptions = {
       from: email,
-      to: process.env.EMAIL_USER,
+      to: "zainali5002@gmail.com",
       subject: `New Contact Form Submission: ${subject}`,
       text: `
         Name: ${name}
@@ -29,10 +29,10 @@ export default async function handler(req, res) {
       await transporter.sendMail(mailOptions);
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (error) {
+      console.error("Error sending email:", error);
       res.status(500).json({ error: "Failed to send email" });
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
-
