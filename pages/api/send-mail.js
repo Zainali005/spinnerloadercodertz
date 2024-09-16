11fd9a7a44c6159e31b711e103b915923baf5e53
Zain Ali <102908271+Zainali005@users.noreply.github.com>
2024-09-16 10:23:46 +0500

    final changes

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -36,3 +36,36 @@ export default async function handler(req, res) {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
+
+// import sgMail from '@sendgrid/mail';
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// export default async function handler(req, res) {
+//   if (req.method === 'POST') {
+//     const { name, email, phone, subject, message } = req.body;
+
+//     const mailOptions = {
+//       to: process.env.EMAIL_USER,
+//       from: email,
+//       subject: `New Contact Form Submission: ${subject}`,
+//       text: `
+//         Name: ${name}
+//         Email: ${email}
+//         Phone: ${phone}
+//         Subject: ${subject}
+//         Message: ${message}
+//       `,
+//     };
+
+//     try {
+//       await sgMail.send(mailOptions);
+//       res.status(200).json({ message: 'Email sent successfully!' });
+//     } catch (error) {
+//       console.error('Error sending email:', error.response.body);
+//       res.status(500).json({ error: 'Failed to send email' });
+//     }
+//   } else {
+//     res.status(405).json({ error: 'Method not allowed' });
+//   }
+// }
