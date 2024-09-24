53be48d9c802989a24888a402cbd225ca37b422d
Zainali005 <zainali5002@gmail.com>
2024-09-24 15:28:51 +0500

    commit

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -1,26 +1,30 @@
-import nodemailer from "nodemailer";
-import { NextResponse } from "next/server";
+import nodemailer from 'nodemailer';
+
 export async function POST(request) {
   const formData = await request.formData();
-  const name = formData.get("name");
-  const phone = formData.get("phone");
-  const position = formData.get("position");
-  const experience = formData.get("experience");
-  const coverLetter = formData.get("coverLetter");
-  const email = formData.get("email");
-  const resumeFile = formData.get("resume");
+  
+  const name = formData.get('name');
+  const phone = formData.get('phone');
+  const position = formData.get('position');
+  const experience = formData.get('experience');
+  const coverLetter = formData.get('coverLetter');
+  const email = formData.get('email');
+  const resumeFile = formData.get('resume');
+
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: 'Gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
-  const adminEmail = "zainali5002@gmail.com";
+
+  const adminEmail = 'zainali5002@gmail.com';
+
   const adminMailOptions = {
     from: process.env.EMAIL_USER,
     to: adminEmail,
-    subject: "New Job Application",
+    subject: 'New Job Application',
     text: `You have received a new job application from ${name}.\n\nDetails:\nName: ${name}\nPhone: ${phone}\nPosition: ${position}\nExperience: ${experience}\nCover Letter: ${coverLetter}\nEmail: ${email}`,
     attachments: [
       {
@@ -29,21 +33,20 @@ export async function POST(request) {
       },
     ],
   };
+
   const userMailOptions = {
     from: process.env.EMAIL_USER,
-    to: "zainali5002@gmail.com",
-    subject: "Application Received",
-    text: `Hi ${name},\n\nThank you for your application for the position of ${position}. We will get back to you shortly.\n\nBest Regards,\nCodeErtz`,
+    to: email,
+    subject: 'Application Received',
+    text: `Hi ${name},\n\nThank you for your application for the position of ${position}. We will get back to you shortly.\n\nBest Regards,\nYour Company Name`,
   };
+
   try {
     await transporter.sendMail(adminMailOptions);
     await transporter.sendMail(userMailOptions);
-    return NextResponse.json({ message: "Emails sent successfully!" });
+    return new Response(JSON.stringify({ message: 'Emails sent successfully!' }), { status: 200 });
   } catch (error) {
-    console.error("Error sending email:", error);
-    return NextResponse.json(
-      { error: "Failed to send emails" },
-      { status: 500 }
-    );
+    console.error('Error sending email:', error);
+    return new Response(JSON.stringify({ error: 'Failed to send emails' }), { status: 500 });
   }
 }
